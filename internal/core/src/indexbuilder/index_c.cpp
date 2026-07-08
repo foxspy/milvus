3c4e6a86dd9c8081f05fc731de53ea4277836c9e
xianliang.li <xianliang.li@zilliz.com>
2026-07-08 16:24:39 +0800

    optimize search
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- internal/core/src/indexbuilder/index_c.cpp
+++ internal/core/src/indexbuilder/index_c.cpp
@@ -105,6 +105,7 @@ class GlobalHeadIndexFileManager : public milvus::FileManager {
             std::make_shared<milvus::storage::RemoteInputStream>(
                 std::move(remote_file.ValueOrDie())));
     }
+    
 
     std::shared_ptr<milvus::OutputStream>
     OpenOutputStream(const std::string&) override {
